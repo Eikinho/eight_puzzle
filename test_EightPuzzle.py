8e6c7a4300133817a9c039b44074ab20ee5e619c
eiki <eiki@pop-os.localdomain>
2022-03-29 22:48:12 -0300

    assert with show_path() at pytests

--- test_EightPuzzle.py
+++ test_EightPuzzle.py
@@ -18,7 +18,7 @@ def testFacil():
   print("=======================================")
   print("Teste Facil 1")
   print(f"Tempo de resolucao: {tf - ts}")
-  assert result.state.env() == str(goal)
+  assert result.show_path() == " ; right ; right ; down ; left ; left ; up ; up ; right ; down"
   
 def testdificil1():
   board = np.array([[7,8,6],[2,3,5],[1,4,0]]) 
@@ -31,7 +31,7 @@ def testdificil1():
   print("=======================================")
   print("Teste Dificil 1")
   print(f"Tempo de resolucao: {tf - ts}")
-  assert result.state.env() == str(goal)
+  assert result.show_path() == " ; up ; left ; left ; up ; right ; down ; down ; left ; up ; up ; right ; right ; down ; left ; down ; left ; up ; right ; up ; left ; down ; down ; right ; up"
 
 def testdificil2():
   board = np.array([[7,8,6],[2,3,5],[0,1,4]]) 
@@ -44,10 +44,10 @@ def testdificil2():
   print("=======================================")
   print("Teste Dificil 2")
   print(f"Tempo de resolucao: {tf - ts}")
-  assert result.state.env() == str(goal)
+  assert result.show_path() == " ; up ; up ; right ; down ; left ; down ; right ; up ; right ; up ; left ; left ; down ; down ; right ; up ; up ; left ; down ; right ; right ; down ; left ; up"
   
 def testdificil3():
-  board = np.array([[8,3,6],[7,5,4],[2,1,0]] )
+  board = np.array([[8,3,6],[7,5,4],[2,1,0]])
   state = EightPuzzle(board, 3, "")
   algorithm = AEstrela()
   ts = time.time()
@@ -57,4 +57,4 @@ def testdificil3():
   print("=======================================")
   print("Teste Dificil 3")
   print(f"Tempo de resolucao: {tf - ts}")
-  assert result.state.env() == str(goal)
+  assert result.show_path() == " ; up ; up ; left ; left ; down ; right ; down ; left ; up ; up ; right ; down ; down ; left ; up ; up ; right ; down ; right ; down ; left ; up"
